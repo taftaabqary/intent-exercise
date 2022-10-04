8b84c2111faec86c289ab466a34d9c589edb6f15
Muhammad Althaaf Abqary <taftaraharjo@gmail.com>
2022-10-04 23:31:33 +0700

    Commit pertama

--- app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
@@ -2,13 +2,51 @@ package id.ac.polinema.intentexercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
+    TextView tvAbout,tvFullname,tvEmail,tvHomepage;
+    Button btnHomepage;
+    CircleImageView imageMasuk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        tvAbout = findViewById(R.id.label_about);
+        tvFullname = findViewById(R.id.label_fullname);
+        tvEmail = findViewById(R.id.label_email);
+        tvHomepage = findViewById(R.id.label_homepage);
+        imageMasuk = findViewById(R.id.image_profile);
+
+        String hasilAbout = getIntent().getExtras().getString("about");
+        String hasilFullname = getIntent().getExtras().getString("fullname");
+        String hasilEmail = getIntent().getExtras().getString("email");
+        final String hasilHomepage = getIntent().getExtras().getString("homepage");
+
+        tvAbout.setText(hasilAbout);
+        tvFullname.setText(hasilFullname);
+        tvEmail.setText((hasilEmail));
+        tvHomepage.setText((hasilHomepage));
+
+
+        btnHomepage = findViewById(R.id.button_homepage);
+        btnHomepage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://"+hasilHomepage;
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/id/ac/polinema/intentexercise/RegisterActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/RegisterActivity.java
@@ -1,14 +1,86 @@
 package id.ac.polinema.intentexercise;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.io.IOException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Button sendData;
+    TextInputEditText etFullName,etEmail,etPassword,etConfPassword,etHomepage,etAbout;
+    CircleImageView avatarImg;
+    ImageButton inputImage;
+
+    private static final String TAG = RegisterActivity.class.getCanonicalName();
+    private static final int GALLERY_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etFullName = findViewById(R.id.text_fullname);
+        etEmail = findViewById(R.id.text_email);
+        etHomepage = findViewById(R.id.text_homepage);
+        etAbout = findViewById(R.id.text_about);
+
+        avatarImg = findViewById(R.id.image_profile);
+        inputImage = findViewById(R.id.imageView);
+
+        inputImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI),GALLERY_REQUEST_CODE);
+            }
+        });
+
+        sendData = findViewById(R.id.button_ok);
+        sendData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindahProfile = new Intent(RegisterActivity.this,ProfileActivity.class);
+                pindahProfile.putExtra("fullname",etFullName.getText().toString());
+                pindahProfile.putExtra("email",etEmail.getText().toString());
+                pindahProfile.putExtra("homepage",etHomepage.getText().toString());
+                pindahProfile.putExtra("about",etAbout.getText().toString());
+                startActivity(pindahProfile);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_CANCELED){
+            Toast.makeText(this, "Cancel Input Image", Toast.LENGTH_SHORT).show();
+            return;
+        }else if(requestCode == GALLERY_REQUEST_CODE){
+            if(data != null){
+                try {
+                    Uri imageUri = data.getData();
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),imageUri);
+                    avatarImg.setImageBitmap(bitmap);
+                }catch (IOException e){
+                    Toast.makeText(this, "Can't  load image", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG,e.getMessage());
+                }
+            }
+        }
     }
 }
